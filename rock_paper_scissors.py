1f6327697aa0adb8758150c9f357c57435a834a7
Daniel Agufenwa <devonaguh33@gmail.com>
2023-12-28 10:37:21 +0000

    fix the response

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,20 +2,32 @@ import random
 
 def play():
     user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+    if user == 'r':
+        print('You Chose Rock')
+    elif user == 'p':
+        print('You Chose Paper') 
+    elif user == 's':
+        print('You Chose Scissors') 
+    
     computer = random.choice(['r', 'p', 's'])
-
+    if computer == 'r':
+            print('Computer Choose Rock')
+    elif computer == 'p':
+            print('Computer Choose Paper') 
+    else:
+        print('Computer Choose Scissors')
+        
     if user == computer:
-        print(computer)
-        return 'It\'s a tie'
+        print('It\'s a tie')
         return play()
-
+    
+    
     # r > s, s > p, p > r
     if is_win(user, computer):
-        print(computer)
         return 'You won!'
     
-    return print(computer)
-    return 'You lost!'
+    
+    print('You lost!')
     return play()
 
 def is_win(player, opponent):
